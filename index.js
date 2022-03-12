f6d3eeab860feb1a63fc00f23b61753cdbf20ab2
Andrew <andrew@pureaquasprings.com>
2022-03-13 08:02:16 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, name, Sam) {
+    // Alternate using ES6 Spread operators:
+    // return { ...driver, ...{ [key]: value } }
+    return Object.assign({}, employee, { [name]: Sam });
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, Andy) {
+    employee[name] = Andy;
+  
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, name) {
+    // Alternate using ES6 Spread operators:
+    // const newObj = { ...driver }
+    const newObj = Object.assign({}, employee);
+  
+    delete newObj[name];
+  
+    return newObj;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name];
+  
+    return employee;
+  }
+  
+  
